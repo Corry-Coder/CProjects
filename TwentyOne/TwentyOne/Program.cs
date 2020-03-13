e09ee1e08e9915c3c6488a88957b751421c1b279
Corry <corry1022@gmail.com>
2020-03-13 16:43:26 -0400

    Add project

---
+++ TwentyOne/TwentyOne/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck deck = new Deck();
+
+            //int count = deck.Cards.Count(x => x.Face == Face.Ace);
+
+            //List<Card> newList = deck.Cards.Where(x => x.Face == Face.King).ToList();
+
+            List<int> numberList = new List<int>() { 1, 2, 3, 535, 342, 22, 6 };
+
+            int sum = numberList.Min();
+
+            Console.WriteLine(sum);
+
+
+            //deck.Shuffle(3);
+
+            //foreach (Card card in deck.Cards)
+            //{
+            //    Console.WriteLine(card.Face + " of " + card.Suit);
+            //}
+            //Console.WriteLine(deck.Cards.Count);
+            Console.ReadLine();
+        }
+
+      
+    }
+}
