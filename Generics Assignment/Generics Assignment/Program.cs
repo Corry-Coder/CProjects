c4c889628675f6146d66cc9c0a3193cde10f37d1
Corry <corry1022@gmail.com>
2020-03-13 22:10:41 -0400

    Added Generics Assignment

---
+++ Generics Assignment/Generics Assignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Assignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee = new Employee<string>();
+            employee.Things = new List<string>
+            {
+                "Corry",
+                "Jerry",
+                "Bill"
+            };
+
+            Employee<int> employee1 = new Employee<int>();
+            employee1.Things = new List<int>
+            {
+                2,
+                4,
+                22
+            };
+
+            foreach (string thing in employee.Things)
+            {
+                Console.WriteLine(thing);
+            }
+            
+        }
+    }
+}
