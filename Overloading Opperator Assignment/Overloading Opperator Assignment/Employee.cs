d2188caa8b7c15c3e01f6306f506aa1677c1ed64
Corry <corry1022@gmail.com>
2020-03-13 22:08:17 -0400

    Adding Overloading Operator Assignment

---
+++ Overloading Opperator Assignment/Overloading Opperator Assignment/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading_Opperator_Assignment
+{
+    public class Employee
+    {
+
+        public int Id { get; set; }
+        
+        
+
+
+        public static bool operator==(Employee employee1, Employee employee2)
+        {
+           
+            return employee1.Id == employee2.Id;
+
+        }
+        public static bool operator!=(Employee employee1, Employee employee2)
+        {
+            
+                return employee1.Id != employee2.Id;
+            
+
+        }
+    } 
+}
---
+++ Overloading Opperator Assignment/Overloading Opperator Assignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading_Opperator_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            Employee employee1 = new Employee();
+            employee1.Id = 22;
+            employee.Id = 6;
+            Console.WriteLine(employee1 == employee);
+            Console.ReadLine();
+        }
+    }
+}
