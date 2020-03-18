38aa276feab5925c9277c532b3aee8ce389f4f57
Corry <corry1022@gmail.com>
2020-03-17 21:14:08 -0400

    Added File Assignment

---
+++ FileAssignment/FileAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type in a number.");
+            int number = Convert.ToInt32(Console.ReadLine());
+            File.WriteAllText(@"C:\Users\Corry\Logs\log1.txt", number.ToString());
+            Console.WriteLine(number);
+        }
+    }
+}
