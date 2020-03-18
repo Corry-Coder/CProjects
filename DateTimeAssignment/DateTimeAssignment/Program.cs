2645a5cc68a3d835a05f876650015b30da9d7c61
Corry <corry1022@gmail.com>
2020-03-17 23:48:16 -0400

    Added DateTimeAssignment

---
+++ DateTimeAssignment/DateTimeAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Please choose a number.");
+            int answer = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("In exactly " + answer + " hours it will be " + DateTime.Now.AddHours(answer));
+            Console.ReadLine();
+        }
+    }
+}
