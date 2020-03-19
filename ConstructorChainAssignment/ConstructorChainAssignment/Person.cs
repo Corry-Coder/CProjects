7c48d37072b24aba7b80d94410edbdb4a4e1466b
Corry <corry1022@gmail.com>
2020-03-19 08:12:15 -0400

    Added Constructor Assignment

---
+++ ConstructorChainAssignment/ConstructorChainAssignment/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorChainAssignment
+{
+    public class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        
+        public Person(string firstName) : this(firstName, "Success")
+        {
+        }
+        public Person(string firstName,  string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+
+        }
+
+        public void SayName()
+        {
+            Console.WriteLine(firstName + lastName);
+        }
+    }
+}
---
+++ ConstructorChainAssignment/ConstructorChainAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorChainAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person("Corry");
+            person.SayName();
+            Console.ReadLine();
+             
+        }
+    }
+}
