a7820a83652b27cc351cc406e4fc919b9946db7b
Corry <corry1022@gmail.com>
2020-03-19 20:14:58 -0400

    Added Exception Assignment

---
+++ AdvancedExceptions/AdvancedExceptions/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedExceptions
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter your age.");
+                int age = Convert.ToInt32(Console.ReadLine());
+                var yearOfBirth = DateTime.Now.Year - age;
+                if (age == 0 || age < 0)
+                {
+                    throw new NegativeZeroException();
+                }
+            }
+            catch (NegativeZeroException)
+            {
+                Console.WriteLine("Please don't enter a leading zero or negative numbers");
+                Console.ReadLine();
+                return;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An error occurred. Please contact your System Administrator.");
+                Console.ReadLine();
+                return;
+            }
+        }
+    }
+}
