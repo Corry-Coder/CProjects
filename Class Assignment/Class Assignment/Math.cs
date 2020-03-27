e1286e8490d8eefc00a38b249ce1475d75bc49a4
Corry <corry1022@gmail.com>
2020-03-27 17:47:24 -0400

    Added Assignment

---
+++ Class Assignment/Class Assignment/Math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Assignment
+{
+    public class Math
+    {
+        public string Operations(string data)
+        {
+            string sum = data + "Hello";
+            return sum;
+        }
+
+        public decimal Operations(decimal product)
+        {
+            decimal answer = product * 100;
+            return answer;
+        }
+
+        public int Operations(int difference)
+        {           
+            int number = difference - 20;
+            return number;
+        }
+    }
+}
---
+++ Class Assignment/Class Assignment/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a name");
+            string data = Console.ReadLine();
+            Math mathClass = new Math();
+            string sum = mathClass.Operations(data);
+            Console.WriteLine(sum);
+
+
+            Console.WriteLine("Please enter a decimal number");
+            decimal product = Convert.ToDecimal(Console.ReadLine());
+            decimal answer = mathClass.Operations(product);
+            Console.WriteLine(answer);
+
+
+            Console.WriteLine("Please enter a number");
+            int difference = Convert.ToInt32(Console.ReadLine());
+            int number = mathClass.Operations(difference);
+            Console.WriteLine(number);
+            Console.ReadLine();
+
+
+        }
+        
+    }
+    
+}
