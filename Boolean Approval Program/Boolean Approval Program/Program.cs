21376505f2e81396a3a7c0a10f478df5270eded2
Corry <corry1022@gmail.com>
2020-03-26 20:55:29 -0400

    Added Boolean Project

---
+++ Boolean Approval Program/Boolean Approval Program/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean_Approval_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? Please answer True or False");
+            bool DUI = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Qualified?");
+            bool isQualified = (age > 15 && DUI == false && tickets <= 3);
+            Console.WriteLine(isQualified);
+            Console.ReadLine();
+        }
+    }
+}
