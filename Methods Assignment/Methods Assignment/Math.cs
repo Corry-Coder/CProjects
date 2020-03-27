f5468624f4501a5093586020d20d7adfef35d313
Corry <corry1022@gmail.com>
2020-03-26 21:13:51 -0400

    Added Method Assignment

---
+++ Methods Assignment/Methods Assignment/Math.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Assignment
+{
+   public class Math
+    {
+        public int Add(int data, int value)
+        {
+            return data + value;
+        }
+
+        public int Subtract(int data, int value)
+        {
+            return data - value;
+        }
+
+        public int Multiply(int data, int value)
+        {
+            return data * value;
+        }
+    }
+}
---
+++ Methods Assignment/Methods Assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Assignment
+{
+    class Program
+    {
+        public static int data { get; set; }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to do math.");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            var mathObject = new Math();
+            var result = mathObject.Add(userInput, 6);
+            result = mathObject.Subtract(result, 10);
+            result = mathObject.Multiply(result, 3);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
