b9dd6e66d178a0528527309e1110cd6a6ebc8ebd
Corry <corry1022@gmail.com>
2020-03-28 15:42:55 -0400

    Update Program.cs

--- Optional Parameters Assignment/Optional Parameters Assignment/Program.cs
+++ Optional Parameters Assignment/Optional Parameters Assignment/Program.cs
@@ -10,19 +10,24 @@ namespace Optional_Parameters_Assignment
     {
         static void Main(string[] args)
         {
+            Math mathClass = new Math();
             Console.WriteLine("Please enter two numbers one at a time. However, you dont have to enter a second number.");
             int num1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter a second number if you wish");
-            string num2 = Console.ReadLine();
-            if (string.IsNullOrEmpty(num2))
-            {
-                return;
-            }
 
-            Math mathClass = new Math();
-            int result = mathClass.Add(num1);
-            Console.WriteLine(result);
-            Console.ReadLine();
+            int num2;
+            if (int.TryParse(Console.ReadLine(), out num2))
+            {
+                int result = mathClass.Add(num1, num2);
+                Console.WriteLine(result);
+                Console.ReadLine();
+            }
+            else
+            {
+                int result = mathClass.Add(num1);
+                Console.WriteLine(result);
+                Console.ReadLine();
+            }
         }
     }
 }
