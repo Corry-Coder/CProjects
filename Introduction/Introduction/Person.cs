53c497898e801cd530e06439cac045ebe5e18096
Corry <corry1022@gmail.com>
2020-04-02 16:02:10 -0400

    Added Assignment

---
+++ Introduction/Introduction/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduction
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string FullName { get; set; }
+    }
+    public class PersonContext : DbContext
+    {
+        public PersonContext() : base("PersonContext")
+        {
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
---
+++ Introduction/Introduction/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new PersonContext())
+            {
+                db.Persons.Add(new Person { FullName = "Corry Woodard" });
+                db.SaveChanges();
+
+                foreach (var person in db.Persons)
+                {
+                    Console.WriteLine(person.FullName);
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
