2e30c358e4c676a61ec14e7b0e38eb1a3af16c19
Corry <corry1022@gmail.com>
2020-04-08 19:53:43 -0400

    Added Abstract assignment

---
+++ Abstract Class Assignment/Abstract Class Assignment/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class_Assignment
+{
+    public abstract class Person
+    {
+        public virtual void SayName() { }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+    
+    
+ }  
+    
+
+
+        
+  
+    
+
