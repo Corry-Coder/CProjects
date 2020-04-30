a499111a39d317909e85f587ef4b09e0855fc795
Corry <corry1022@gmail.com>
2020-04-29 20:45:36 -0400

    Finished changes to program

--- TwentyOne/Casino/Card.cs
+++ TwentyOne/Casino/Card.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TwentyOne
+namespace Casino
 {
     public struct Card
     {
         public Suit Suit { get; set; }
         public Face Face { get; set; }
+        public override string ToString()
+        {
+            return string.Format("{0} of {1}", Face, Suit);
+        }
     }
     public enum Suit
     {
